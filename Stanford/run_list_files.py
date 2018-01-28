95e73476ced52a24755e9df307eae07932731224
AnaelleD <anaelle.dambreville@gmail.com>
2018-01-28 11:57:20 +0100

    maj script stanford

--- Stanford/run_list_files.py
+++ Stanford/run_list_files.py
@@ -10,9 +10,9 @@ import subprocess as sp
 
 # Chemin vers les articles:
 path = "F:/MIASHS/UEs/text_analysis/ActuMap_data/corpus4/"
-os.chdir(path)
 
 # Liste des articles:
+os.chdir(path)
 laListe = os.listdir()
 
 # Liste des articles avec le chemin en entier
@@ -20,9 +20,13 @@ laListeOK=[]
 for l in laListe:
     laListeOK.append(path+l)
 
+# Chemin vers stanford:
+path = "C:/Users/Anaelle/Desktop/info/TEXT_ANALYSIS/stanford-corenlp-full-2017-06-09/"
+os.chdir(path)
+
 # Lancer Standord pour chaque texte :
 # Attention bien placer Python ou Spyder dans le dossier de Stanford corenlp !
-for l in laListeOK[0:2]:
+for l in laListeOK[0:2]: # Enlever [0:2] quand ça marche !!!!!
     print(l)
     toRun = 'java -cp "*" -Xmx1g edu.stanford.nlp.pipeline.StanfordCoreNLP -props samplePropsFr.properties -file '+l+' -outputDirectory output'
     sp.run(toRun)
